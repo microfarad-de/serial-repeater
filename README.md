7d27002bbc82e9ee4e30317b729f8e943d929212
Karim Hraibi <khraibi@apple.com>
2025-02-13 00:14:03 +0100

    Initial upload

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# serial-repeater
-Simple serial port repeater
+# Serial Port Repeater
+
+Arduino serial port repeater used for configuring common bluetooth modules such as the HC-05 or the HM-10.
+
+
+## Notes
+
+The code has been implemented and tested on an Arduino Pro Mini clone board based on the ATmega328P microcontroller with an 8 MHz crystal.
+
+This project uses Git submodules. In order to get its full source code, please clone this Git repository to your local workspace, then execute the follwoing command from within the repository's root directory: `git submodule update --init`.
+
+The following dependencies should to be installed:
+* `pip install pyserial`
+
+This project can be compiled from command line using the via makefile using the following commands:
+
+* Compile: `make`
+* Remove compiled artefacts: `make clean`
+* Upload to Arduino board: `make upload`
---
+++ serial-repeater.ino
@@ -0,0 +1,112 @@
+#include <SoftwareSerial.h>
+
+
+#define SOFT_RX 4
+#define SOFT_TX 5
+
+// Serial baud rate
+// Default for HC-05: 38400 (line ending: NL+CR)
+// Default for HM-10: 9600  (line ending: NL+CR)
+#define SOFT_BAUD_RATE 9600
+#define HARD_BAUD_RATE 9600
+
+
+SoftwareSerial SoftSerial(SOFT_RX, SOFT_TX); // RX, TX
+
+void setup()
+{
+  Serial.begin(HARD_BAUD_RATE);    // hardware serial for the USB-PC
+  SoftSerial.begin(SOFT_BAUD_RATE);  // software serial Arduino to HC-06 (38400 is default)
+  Serial.println("+ + +  S E R I A L  R E P E A T E R  + + +");
+}
+
+
+void loop() {
+  static char c;
+
+  if (SoftSerial.available()) {
+    c = SoftSerial.read();
+    Serial.print(c);
+  }
+
+  if (Serial.available()) {
+    c = Serial.read();
+    SoftSerial.print(c);
+    Serial.print(c);
+  }
+}
+
+
+
+
+/*
+void setup() {
+  Serial.begin(HARD_BAUD_RATE);    // hardware serial for the USB-PC
+  BtSerial.begin(SOFT_BAUD_RATE);  // software serial Arduino to HC-06 (38400 is default)
+
+  // print instructions
+#ifdef HM_10
+  Serial.println("HM-10 AT Command Programmer");
+#else
+  Serial.println("HC-05 AT Command Programmer");
+#endif
+
+  Serial.print("For Arduino Rx use pin ");
+  Serial.println(SOFT_RX);
+
+  Serial.print("For Arduino Tx use pin ");
+  Serial.println(SOFT_TX);
+
+  Serial.println(" -- Command Reference ---");
+  Serial.println("To Read use '?', Like AT+PSWD?");
+  Serial.println("AT (simply checks connection)");
+  Serial.println("AT+VERSION (requests the firmware verison)");
+  Serial.println("AT+ROLE=x (0 =Slave role, 1 =  Master role, 2 = Slave-Loop role  default = 0)");
+  Serial.println("AT+NAME=xxxxx (to change name to xxxxx default=HC-05");
+  Serial.println("AT+PSWD=nnnn (to change password to 4 digit nnnn default = 1234");
+  Serial.println("AT+UART=nnnn,s,p (nnnn=Baud, s=stop bits (0=1, 1=2), p=parity (0=None, 1=Odd, 2=Even)");
+  Serial.println("AT+POLAR=a,b (a=PIO8 (LED), b=PIO9 for both 0=low turn on, 1 = high turn on.");
+  Serial.println("AT+ORGL (reset all parameters to defaults)");
+  Serial.println("AT+RESET (restarts the HC-05. Will not be in AT mode afterward unless button held");
+
+
+}
+
+void loop() {
+  static char c = ' ';
+
+  // Read from HC-05
+  if (BtSerial.available()) {
+    while(BtSerial.available()) {
+      c = BtSerial.read();
+      Serial.print(c);
+      }
+  }
+
+  // Read from PC
+  if (Serial.available()){
+    delay(10);
+    c = Serial.read();
+
+#ifdef HM_10
+    // do not send line end characters to the HM-10
+    if (c != 10 && c != 13 ) {
+         BtSerial.write(c);
+    }
+
+    // Echo the user input to the main window.
+    // If there is a new line print the ">" character.
+    if (c == 10) {
+      Serial.print("\r\n>");
+    }
+    Serial.write(c);
+
+
+#else
+    BtSerial.print(c); // show the HC-05 responce
+    Serial.print(c);   // echo it back to the PC
+#endif
+  }
+}
+
+*/
